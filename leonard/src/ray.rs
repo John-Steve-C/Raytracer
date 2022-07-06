f2940688ac9a90a07229e920e264817d5d00e89a
John-Steve-C <leonardchen701@outlook.com>
2022-07-06 16:07:22 +0800

    image_3

--- leonard/src/ray.rs
+++ leonard/src/ray.rs
@@ -13,9 +13,21 @@ impl Ray {
 }
 
 impl Ray {
+    pub fn hit_sphere(center : Vec3, radius : f64, r : Ray) -> bool{
+        let oc = r.orig - center;
+        let a = Vec3::dot(r.dir, r.dir);
+        let b = 2.0 * Vec3::dot(oc, r.dir);
+        let c = Vec3::dot(oc, oc) - radius * radius;
+        let discriminant = b *b - 4. * a * c;
+        discriminant > 0.
+    } 
+
     pub fn ray_color(r : Ray) -> Vec3{
+        if Ray::hit_sphere(Vec3::new(0., 0., -1.), 0.5, r) {
+            return Vec3::new(1., 0., 0.);
+        }
         let unit_dir = Vec3::unit_vector(r.dir);
         let t = 0.5 * (unit_dir.y + 1.0);
-        Vec3::new(1., 1., 1.) * (1. - t) + Vec3::new(0.5, 0.7, 1.) * t
+        return Vec3::new(1., 1., 1.) * (1. - t) + Vec3::new(0.5, 0.7, 1.) * t;
     }
 }
