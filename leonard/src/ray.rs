f0b374a2092254a50995eab9c49a44a8504d20cf
John-Steve-C <leonardchen701@outlook.com>
2022-07-06 16:52:38 +0800

    readme_updated

--- leonard/src/ray.rs
+++ leonard/src/ray.rs
@@ -13,18 +13,25 @@ impl Ray {
 }
 
 impl Ray {
-    pub fn hit_sphere(center : Vec3, radius : f64, r : Ray) -> bool{
+    pub fn hit_sphere(center : Vec3, radius : f64, r : Ray) -> f64{
         let oc = r.orig - center;
         let a = Vec3::dot(r.dir, r.dir);
         let b = 2.0 * Vec3::dot(oc, r.dir);
         let c = Vec3::dot(oc, oc) - radius * radius;
         let discriminant = b *b - 4. * a * c;
-        discriminant > 0.
+        if discriminant < 0. { 
+            -1.
+        } else {
+            (- b - discriminant.sqrt()) / (2. * a) //求解出具体的根
+        }
     } 
 
     pub fn ray_color(r : Ray) -> Vec3{
-        if Ray::hit_sphere(Vec3::new(0., 0., -1.), 0.5, r) {
-            return Vec3::new(1., 0., 0.);
+        let t =  Ray::hit_sphere(Vec3::new(0., 0., -1.), 0.5, r);
+        if t > 0. {
+            let n = Vec3::unit_vector(r.at(t) -  Vec3::new(0., 0., -1.));
+            //求出法向量
+            return Vec3::new(n.x + 1., n.y + 1., n.z + 1.) * 0.5;
         }
         let unit_dir = Vec3::unit_vector(r.dir);
         let t = 0.5 * (unit_dir.y + 1.0);
